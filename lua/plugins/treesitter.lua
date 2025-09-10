7f931e877312817e14a42d6c1795179e87d3b97f
Tarcisio Brito <tarcisio.brito@ensino.inca.gov.br>
2025-09-10 12:35:17 -0300

    update  2025-09-10 12:35:17

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,21 @@
 return{
-	"nvim-treesitter/nvim-treesitter",
-	branch = 'master',
-	lazy = false,
-	build = ":TSUpdate"
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
+    build = ":TSUpdate",
+    opts = {
+      ensure_installed = { 'bash', 'lua', 'markdown', 'vim', 'vimdoc' },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'markdown' },
+      },
+      indent = { enable = true },
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+    end
 }
