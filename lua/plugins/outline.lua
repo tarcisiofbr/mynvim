087ff262f647728458bf2b4f21053afbaeb7c0d8
Tarcisio Brito <tarcisio.brito@ensino.inca.gov.br>
2025-09-23 11:14:55 -0300

    removed icons of outline (i believe)

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -1,26 +1,29 @@
 return {
-  "hedyhli/outline.nvim",
-  config = function()
-    -- Example mapping to toggle outline
-    vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
-      { desc = "Toggle Outline" })
+    "hedyhli/outline.nvim",
+    config = function()
+        -- Example mapping to toggle outline
+        vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
+            { desc = "Toggle Outline" })
 
-    require("outline").setup {
-      -- Your setup opts here (leave empty to use defaults)
-	  providers = {
-    priority = { 'markdown' },
-    -- Configuration for each provider (3rd party providers are supported)
-    lsp = {
-      -- Lsp client names to ignore
-      blacklist_clients = {},
-    },
-    markdown = {
-      -- List of supported ft's to use the markdown provider
-      filetypes = {'markdown', "quarto"},
-    },
-  },
+        require("outline").setup {
+            -- Your setup opts here (leave empty to use defaults)
+            providers = {
+                priority = { 'markdown' },
+                -- Configuration for each provider (3rd party providers are supported)
+                lsp = {
+                    -- Lsp client names to ignore
+                    blacklist_clients = {},
+                },
+                markdown = {
+                    -- List of supported ft's to use the markdown provider
+                    filetypes = {'markdown', "quarto"},
+                },
+            },
+            symbols = {
+                icon_fetcher = function() return "" end,
+            }
 
 
-    }
-  end,
+        }
+    end,
 }
